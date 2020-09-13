fd3917a59a2a172d46c5a51e4d08c9f04077d036
Josh Richards <desert123100@hotmail.com>
2020-09-12 19:27:20 -0700

    Nathria included

--- lib/helper.py
+++ lib/helper.py
@@ -7,14 +7,30 @@ class SigHelper:
     
     def __init__(self):
         pass
+    
+    def getCurrentBoss(self, tier):
+        with open(os.path.join(self.here,'prog.json')) as json_file:
+            data = json.load(json_file)
+            return_data = {}
+            for difficulty in data['tiers'][tier]:
+                temp = data['tiers'][tier][difficulty]
+                for boss in temp:
+                    if temp[boss]['best'] != '0%':
+                        return_data['boss'] = boss
+                        return_data['difficulty'] = difficulty
+                        return return_data
+            return_data['boss'] = boss
+            return_data['difficulty'] = difficulty
+            return return_data
 
     def getAllBossData(self, tier, difficulty):
         with open(os.path.join(self.here,'prog.json')) as json_file:
             return_data = ''
             data = json.load(json_file)
+            return_data += difficulty + ':\n'
             for boss, value in data['tiers'][tier][difficulty].items():
                 #return_data += boss + '\t\t' + value['best'] + '\t' + value['wipes'] + ' wipes\n'
-                return_data += ('{} {} {} wipes\n').format(boss, value['best'],value['wipes'])
+                return_data += ('{} {} {} wipes {}\n').format(boss, value['best'],value['wipes'],value['log'])
             return return_data
     
     def getBossData(self, tier, difficulty, boss):
